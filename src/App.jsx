faf2980b70d689e2a85a20549c6198f2d5ee37c1
franpd8 <102958579+franpd8@users.noreply.github.com>
2022-06-29 19:42:09 +0200

    profile componente added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Register from './components/Register/Register';
 import Login from './components/Login/Login';
 import Home from "./components/Home/Home";
 import Header from './components/Header/Header';
+import Profile from './components/Profile/Profile';
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,40 +1,44 @@
-
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { logout } from "../../features/auth/authSlice";
 
 const Header = () => {
+  const { user } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const { user } = useSelector((state) => state.auth);
-    const navigate= useNavigate()
-    const dispatch= useDispatch()
+  const onLogout = (e) => {
+    e.preventDefault();
+    dispatch(logout());
+    navigate("/login");
+  };
 
-    const onLogout = (e) => {
-      e.preventDefault();
-      dispatch(logout());
-      navigate("/login");
-    };
-    
-  
-
-    return (
-      <nav>
+  return (
+    <nav>
       <span>header</span>
       <div>
-        {user ? 
-          <span>
-            <Link to="/" onClick={onLogout}>Logout</Link>
-          </span>
-         : 
+        {user ? (
           <>
-            <span><Link to="/login">Login</Link></span>
-            <span><Link to="/register">Register</Link></span>
+            <span>
+              <Link to="/" onClick={onLogout}>Logout</Link>
+            </span>
+            <span>
+              <Link to="/profile">{user.user.name}</Link>{" "}
+            </span>
           </>
-        }
+        ) : (
+          <>
+            <span>
+              <Link to="/login">Login</Link>
+            </span>
+            <span>
+              <Link to="/register">Register</Link>
+            </span>
+          </>
+        )}
       </div>
     </nav>
+  );
+};
 
-    )
-  }
-  
-  export default Header
+export default Header;
