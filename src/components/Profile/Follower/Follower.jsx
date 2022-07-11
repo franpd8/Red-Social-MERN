cf7cb23488122d82857d784dbd4ef8bd7418e5dd
franpd8 <102958579+franpd8@users.noreply.github.com>
2022-07-11 08:28:30 +0200

    styled profile ok

--- src/components/Profile/Follower/Follower.jsx
+++ src/components/Profile/Follower/Follower.jsx
@@ -1,20 +1,23 @@
 import { Link } from "react-router-dom";
 function Follower({ userData }) {
   const follower = userData.followers?.map((user) => {
+    const avatar = user.avatar;
     return (
       <div className="follower__user" key={user._id}>
         <Link to={"/user/" + user._id}>
-          <p> {user.name}</p>
-          <p>
-            <img className="smallIcon__img" src={user.avatar} />
-          </p>
+        <div className="userImg">
+            <img
+              className="img"
+              src={avatar ? avatar : "https://i.imgur.com/Svw4Sam.png"}
+            />
+          </div>
+          <div className="userName"> {user.name}</div>
         </Link>
       </div>
     );
   });
   return (
     <div className="follower">
-      <p>My followers</p>
       <div className="follower__box">{follower}</div>
     </div>
   );
--- src/components/Profile/Following/Following.jsx
+++ src/components/Profile/Following/Following.jsx
@@ -1,23 +1,25 @@
 import { Link } from "react-router-dom";
 function Following({ userData }) {
   const following = userData.following?.map((user) => {
+    const avatar = user.avatar;
     return (
       <div className="following__user" key={user._id}>
         <Link to={"/user/" + user._id}>
-          <p> {user.name}</p>
-          <p>
-            <img className="smallIcon__img" src={user.avatar} />
-          </p>
+          <div className="userImg">
+            <img
+              className="img"
+              src={avatar ? avatar : "https://i.imgur.com/Svw4Sam.png"}
+            />
+          </div>
+          <div className="userName"> {user.name}</div>
         </Link>
       </div>
     );
   });
   return (
     <div className="following">
-      <p>im following</p>
       <div className="following__box">{following}</div>
     </div>
   );
 }
-
 export default Following;
